5dd7cc4338d6eab8846581c7bcf6c872337e1eeb
samuel <samuelmmanor@gmail.com>
2022-01-17 14:45:07 -0500

    Completed assignment + delete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+  })
 });
+
+function buildToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = 'x';
+  li.textContent = todo;
+  li.appendChild(btn);
+  document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
