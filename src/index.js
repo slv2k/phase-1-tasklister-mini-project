a83d2250d9a969b26569f747bf00524e6ee4b359
samuel <samuelmmanor@gmail.com>
2022-01-17 20:33:03 -0500

    Completed assignment, added description option

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("form").addEventListener('submit', (e) => {
     e.preventDefault();
-    buildToDo(e.target.new_task_description.value);
+    buildToDo(e.target.new_task_title.value, e.target.new_task_description.value);
   })
 });
 
-function buildToDo(todo) {
-  let li = document.createElement('li');
+function buildToDo(title, desc) {
+  // element initializing
+  let liTitle = document.createElement('li');
+  let liDesc = document.createElement('li');
   let btn = document.createElement('button');
+
+  // element creation
   btn.addEventListener("click", handleDelete);
   btn.textContent = 'x';
-  li.textContent = todo;
-  li.appendChild(btn);
-  document.querySelector('#tasks').appendChild(li);
+  liTitle.textContent = title;
+  liDesc.textContent = desc;
+
+  // element appending
+  liDesc.appendChild(btn);
+  liTitle.appendChild(liDesc);
+  document.querySelector('#tasks').appendChild(liTitle);
+
 }
 
 function handleDelete(e) {
-  e.target.parentNode.remove();
+  e.target.parentNode.parentNode.remove();
 }
